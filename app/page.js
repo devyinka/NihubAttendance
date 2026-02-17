283c9eb85c023fb6b377dced4ae1ec025b4ffd3f
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-17 13:30:19 +0100

    mThe message "modified: app/page.js" indicates that the file "page.js" in the "app" directory has been modified. This typically means that changes have been made to the content of the file since the last commit.     odified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -79,8 +79,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="text-[#7741c3] text-center font-mono text-xs font-normal justify-center  absolute bg-white place-self-center w-[80%] flex flex-wrap flex-col lg:mt-0 lg:pt-8  ">
-        <h1 className="text-sm font-bold md:text-2xl lg:text-3xl   lg:mt-[-24px] lg:pb-6">
+      <div className="text-[#7741c3] text-center font-mono text-xs font-normal justify-center  absolute bg-white place-self-center w-[80%] flex flex-wrap flex-col mt-2 lg:mt-2 lg:pt-0   ">
+        <h1 className="text-sm font-bold md:text-2xl lg:text-3xl pt-0 pb-7">
           Available Events
         </h1>
         <h3 className="text-[70%] md:text-[100%] lg:text-[120%] mb-8">
